7e383fdf4178b9aa3244cba52f531ca683665977
ArtemSoldatkin <artemsoldatkin@yandex.ru>
2026-02-17 08:09:37 +0100

    T-17 - Added goroutiine timeout into recoverStuckDeliveryAttempts

--- internal/delivery_engine/delivery_recovery_engine.go
+++ internal/delivery_engine/delivery_recovery_engine.go
@@ -9,18 +9,20 @@ import (
 )
 
 // RecoverStuckDeliveryAttempts identifies and resets delivery attempts that have been in-flight for too long, allowing them to be retried by the delivery engine.
-func recoverStuckDeliveryAttempts(ctx context.Context, svc *service.Service) error {
+func recoverStuckDeliveryAttempts(svc *service.Service) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second) // TODO make timeout configurable
+	defer cancel()
+
 	return svc.RecoverStuckDeliveryAttempts(ctx)
 }
 
 // StartRecoveryEngine initializes and starts the delivery recovery engine, which periodically checks for stuck delivery attempts and resets them for retrying.
 func StartRecoveryEngine(svc *service.Service, interval time.Duration) {
 	logrus.Info("Starting delivery recovery engine...")
-	ctx := context.Background()
 	ticker := time.NewTicker(interval)
 	defer ticker.Stop()
 	for {
-		if err := recoverStuckDeliveryAttempts(ctx, svc); err != nil {
+		if err := recoverStuckDeliveryAttempts(svc); err != nil {
 			logrus.Error("Error recovering stuck delivery attempts:", err)
 		}
 		<-ticker.C
