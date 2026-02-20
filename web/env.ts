49cd82874944bfd65098de643374ee2b87808d1c
ArtemSoldatkin <artemsoldatkin@yandex.ru>
2026-02-20 07:44:10 +0100

    T-20 - Added API_PROTOCOL and API_HOST envs and paramterized any usage of api

--- web/env.ts
+++ web/env.ts
@@ -6,6 +6,8 @@ import * as z from 'zod';
 
 const envSchema = z.object({
 	ENV: z.enum(['dev', 'uat', 'prod']).default('dev'),
+	API_PROTOCOL: z.enum(['http', 'https']).default('http'),
+	API_HOST: z.string().default('localhost'),
 	API_PORT: z.string().default('3000')
 });
 
