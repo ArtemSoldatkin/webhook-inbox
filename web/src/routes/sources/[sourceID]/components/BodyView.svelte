d918570e58c0ad15372ce57832a87280531e0bea
ArtemSoldatkin <artemsoldatkin@yandex.ru>
2026-02-22 08:29:18 +0100

    T-21 - Combined display types into 1

--- web/src/routes/sources/[sourceID]/components/BodyView.svelte
+++ web/src/routes/sources/[sourceID]/components/BodyView.svelte
@@ -5,11 +5,7 @@
 
 <section>
 	<h3>Request body</h3>
-	{#if contentType.startsWith('application/json')}
-		<pre>{atob(body)}</pre>
-	{:else if contentType.startsWith('application/x-www-form-urlencoded')}
-		<pre>{atob(body)}</pre>
-	{:else if contentType.startsWith('text/plain')}
+	{#if contentType.startsWith('application/json') || contentType.startsWith('application/x-www-form-urlencoded') || contentType.startsWith('text/plain')}
 		<pre>{atob(body)}</pre>
 	{:else}
 		<p>Unsupported content type: {contentType}</p>
