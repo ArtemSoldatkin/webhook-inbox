2f3449ad37159683c8880e1b6169a63337a1d3c6
ArtemSoldatkin <artemsoldatkin@yandex.ru>
2026-03-21 07:57:49 +0100

    T-31 - Distinguish between body and empty body on UII

--- web/src/routes/sources/[sourceID]/components/BodyView.svelte
+++ web/src/routes/sources/[sourceID]/components/BodyView.svelte
@@ -11,26 +11,58 @@
 		contentType?: ContentType;
 	};
 
-	let { body, contentType }: Props = $props();
+	type ParsedBody = {
+		content: string | null;
+		error: string | null;
+	};
 
-	const parsedBody = $derived(body ? atob(body) : '');
+	let { body, contentType }: Props = $props();
+	const parsedBody = $derived(parseBody(body));
+
+	function parseBody(body: string | undefined): ParsedBody {
+		if (!body)
+			return {
+				content: null,
+				error: null
+			};
+
+		try {
+			return {
+				content: atob(body),
+				error: null
+			};
+		} catch (err) {
+			const errorMessage = err instanceof Error ? err.message : String(err);
+			console.error(errorMessage);
+
+			return {
+				content: body,
+				error: 'Failed to parse body as JSON, falling back to raw string'
+			};
+		}
+	}
 </script>
 
 <section>
 	<h3>Request body</h3>
-	{#if !body}
-		<p>No body content</p>
-	{:else if !contentType}
+	{#if !contentType}
 		<p>Content type unknown, cannot display body</p>
+	{:else if parsedBody.error !== null && parsedBody.content === null}
+		<p>{parsedBody.error}</p>
+	{:else if parsedBody.error !== null}
+		<p>{parsedBody.error}</p>
+		<p>Original body: {parsedBody.content}</p>
+	{:else if parsedBody.content === null}
+		<p>Body content is empty</p>
 	{:else if contentType.startsWith('application/json')}
-		<JSONBodyView body={parsedBody} />
+		<JSONBodyView body={parsedBody.content} />
 	{:else if contentType.startsWith('application/x-www-form-urlencoded')}
-		<FormUrlEncodedBodyView body={parsedBody} />
+		<FormUrlEncodedBodyView body={parsedBody.content} />
 	{:else if contentType.startsWith('application/xml')}
-		<XMLBodyView body={parsedBody} />
+		<XMLBodyView body={parsedBody.content} />
 	{:else if contentType.startsWith('text/plain')}
-		<PlainTextBodyView body={parsedBody} />
+		<PlainTextBodyView body={parsedBody.content} />
 	{:else}
-		<ByteBodyView body={parsedBody} {contentType} />
+		<ByteBodyView body={parsedBody.content} {contentType} />
 	{/if}
 </section>
